16bb78e01dc1999577f0f9ebf8354adbf0c1ff05
Ashwini <salunkheashwini1998@gmail.com>
2023-01-12 16:15:04 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -7,10 +7,11 @@ namespace LinkedListAssignment
         public static void Main(string[] args)
         {
             LinkedList linkedList = new LinkedList();
-            linkedList.Add(56);
-            linkedList.Add(30);
             linkedList.Add(70);
+            linkedList.Add(30);
+            linkedList.Add(56);
             linkedList.Display();
         }
     }
 }
+
