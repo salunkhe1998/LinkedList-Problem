2be98617128684b8e8fa4a3df39886365bbeedfa
Ashwini <salunkheashwini1998@gmail.com>
2023-01-12 21:34:24 +0530

    thisis file

--- Program.cs
+++ Program.cs
@@ -11,6 +11,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace LinkedListAssignment
 {
@@ -20,18 +26,16 @@ namespace LinkedListAssignment
         {
             LinkedList linkedList = new LinkedList();
             linkedList.Add(56);
-            //linkedList.Add(30);
+            linkedList.Add(30);
             linkedList.Add(70);
             linkedList.Display();
 
-            linkedList.InsertAtPerticularPosition(2, 30);
-            linkedList.Display();
-            linkedList.pop();
+            // linkedList.InsertAtPerticularPosition(2, 30);
+            //linkedList.Display();
+            linkedList.popEnd();
             linkedList.Display();
             Console.ReadLine();
 
         }
     }
 }
-Footer
-© 2023 GitHub, Inc.
