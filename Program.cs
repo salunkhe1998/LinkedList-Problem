d4bb452b31014470abf87453e4fbcaf1ab23d819
Ashwini <salunkheashwini1998@gmail.com>
2023-01-12 15:51:44 +0530

    mehod calling

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace LinkedListAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+        }
+    }
+}
